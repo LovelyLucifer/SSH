4fbcbea7a7debdab15ae57469da2b120302eecde
LovelyLucifer <953907491@qq.com>
2015-09-17 17:33:40 +0800

    sshtest

---
+++ src/dao/DepartmentDao.java
@@ -0,0 +1,14 @@
+package dao;
+
+import java.util.List;
+
+import entity.Department;
+
+public class DepartmentDao extends BaseDao{
+
+	public List<Department> getAll(){
+		String hql = "FROM Department";
+		return getSession().createQuery(hql).list();
+	}
+	
+}
---
+++ src/service/DepartmentService.java
@@ -0,0 +1,20 @@
+package service;
+
+import java.util.List;
+
+import dao.DepartmentDao;
+import entity.Department;
+
+public class DepartmentService {
+	
+	private DepartmentDao departmentDao;
+	
+	public void setDepartmentDao(DepartmentDao departmentDao) {
+		this.departmentDao = departmentDao;
+	}
+	
+	public List<Department> getAll(){
+		return departmentDao.getAll();
+	}
+	
+}
